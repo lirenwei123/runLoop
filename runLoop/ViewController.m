0720fa49a6a1b2db78472b24335c66c4b8530166
Lirw <303624150@qq.com>
2018-12-27 01:30:21 +0800

    测试
    第一次提交

--- runLoop/ViewController.m
+++ runLoop/ViewController.m
@@ -10,8 +10,9 @@
 
 typedef void(^runLoopBlock)();
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
 
+@property(nonatomic,strong)UITableView *tab;
 
 @end
 
@@ -24,31 +25,95 @@ typedef void(^runLoopBlock)();
     [_tasks addObject:task];
 }
 
+-(UITableView *)tab{
+    if (!_tab) {
+        _tab = [[UITableView alloc]initWithFrame:CGRectMake(0, 20, 414, 716) style:UITableViewStylePlain];
+        _tab.delegate = self;
+        _tab.dataSource = self;
+        _tasks = @[].mutableCopy;
+    }
+    return _tab;
+}
+
 
 -(void)addRunloopObserver{
     CFRunLoopRef runloopRef = CFRunLoopGetCurrent();
-    CFRunLoopObserverContext contex = {
+//    CFRunLoopObserverContext contex = {
+//        0,
+//        (__bridge void *)self,
+//        &CFRetain,
+//        &CFRelease,
+//        NULL
+//    };
+//    CFRunLoopObserverRef runloopObserver = CFRunLoopObserverCreate(NULL, kCFRunLoopBeforeWaiting, YES, 0, &callBack, &contex);
+//    CFRunLoopAddObserver(runloopRef, runloopObserver, kCFRunLoopCommonModes);
+//    CFRelease(runloopObserver);
+    
+    
+    CFRunLoopTimerContext timeContex = {
         0,
         (__bridge void *)self,
         &CFRetain,
         &CFRelease,
         NULL
     };
-    CFRunLoopObserverRef runloopObserver = CFRunLoopObserverCreate(NULL, kCFRunLoopBeforeWaiting, YES, 0, &callBack, &contex);
-    CFRunLoopAddObserver(runloopRef, runloopObserver, kCFRunLoopCommonModes);
-    CFRelease(runloopObserver);
+
+  CFRunLoopTimerRef timeRef =  CFRunLoopTimerCreate(NULL, 0, 0, kCFRunLoopBeforeWaiting, 1, &TimerCallBack, &timeContex);
+    CFRunLoopAddTimer(runloopRef, timeRef, kCFRunLoopCommonModes);
+    CFRelease(timeRef);
 }
 
+
+
 void callBack(CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info){
-    UIViewController *vc  =(__bridge UIViewController *)info;
-}
+    ViewController *vc = (__bridge ViewController*)info;
+    [vc.tasks enumerateObjectsUsingBlock:^(runLoopBlock obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        obj();
+    }];
     
+
+}
+
+void TimerCallBack(CFRunLoopTimerRef timer, void *info){
+    ViewController *vc = (__bridge ViewController*)info;
+    [vc.tasks enumerateObjectsUsingBlock:^(runLoopBlock obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        obj();
+        
+    }];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self addRunloopObserver];
+    [self.view addSubview:self.tab];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 100;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellid = @"id";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellid];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellid];
+    }
+    runLoopBlock block = ^{
+    
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                NSLog(@"-----%@",[NSThread currentThread]);
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    cell.textLabel.text = [NSString stringWithFormat:@"%ld",indexPath.row];
+                });
+            });
+      
+    };
+    [self.tasks addObject:block];
+    return cell;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
